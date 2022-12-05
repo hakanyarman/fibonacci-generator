0e42ce4f400f0d1d81164d2519c678254e15ffaf
hakanyarman <104122864+hakanyarman@users.noreply.github.com>
2022-12-05 23:07:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 function fibonacciGenerator(n) {
     let array = [0, 1];
-    for (let i = 0; i < n - 2; i++) {
-        let nextNum = array[array.length - 1] + array[array.length - 2];
-        array.push(nextNum);
+    if(n>2){
+        for (let i = 0; i < n - 2; i++) {
+            let nextNum = array[array.length - 1] + array[array.length - 2];
+            array.push(nextNum);
+        }
+    }
+    else if(n==1){
+        array = [0];
+    }
+    else if(n==2){
+        array = array ;
     }
     return array;
 };
-fibonacciGenerator(8);
-fibonacciGenerator(12);
+
 // n is the number of elements in the fibonacci array.
